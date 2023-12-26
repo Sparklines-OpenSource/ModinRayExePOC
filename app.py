35a3767dded4eb35e570828e2908f6d01c67731f
MahdiALLALA <mahdi.allala@sparklines.io>
2023-12-26 22:19:33 +0100

    removed logger code

--- app.py
+++ app.py
@@ -1,28 +1,14 @@
 from flask import Flask
 import modin.pandas as pd
 import ray
-#from ray import logger
-import logging
 
-logger = logging.getLogger("ray")
-ray_data_logger = logging.getLogger("ray.data")
-ray_tune_logger = logging.getLogger("ray.tune")
-ray_rllib_logger = logging.getLogger("ray.rllib")
-ray_train_logger = logging.getLogger("ray.train")
-ray_serve_logger = logging.getLogger("ray.serve")
-logger.handlers.clear()
-ray_data_logger.handlers.clear()
-ray_tune_logger.handlers.clear()
-ray_rllib_logger.handlers.clear()
-ray_serve_logger.handlers.clear()
-ray_train_logger.handlers.clear()
-#logger.addHandler(logging.FileHandler('ray_logs.log',mode="w"))
+
 
 
 app = Flask(__name__)
 
 
-@app.route('/read_data', methods=['GET'])
+@app.route('/', methods=['GET'])
 def read_data():
     file_path = "titanic.csv"
     modin_df = pd.read_csv(file_path)
